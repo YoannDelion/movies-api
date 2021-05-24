47b0c40ceccb5d4ec3701e29aa9c505357e8db9c
Yoann Delion <yoanndelion@gmail.com>
2021-05-24 16:37:13 +0200

    signup route

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,28 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const express = require('express')
+const firebase = require('firebase')
 
 admin.initializeApp()
 const app = express()
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC_T0AFIyR_8Uowg82Ddpas1qtgycViT6o",
+  authDomain: "movies-181e8.firebaseapp.com",
+  projectId: "movies-181e8",
+  storageBucket: "movies-181e8.appspot.com",
+  messagingSenderId: "1024435590017",
+  appId: "1:1024435590017:web:2400e1f6659cc133720e80",
+  measurementId: "G-CF881YXC9E"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const db = admin.firestore()
+
 
 app.get('/reviews', (request, response) => {
-  admin.firestore().collection('reviews').orderBy('createdAt', 'desc').get()
+  db.collection('reviews').orderBy('createdAt', 'desc').get()
     .then(data => {
       const reviews = []
       data.forEach(doc => {
@@ -36,12 +51,60 @@ app.post('/review', (request, response) => {
     updatedAt: new Date().toISOString()
   }
 
-  admin.firestore().collection('reviews').add(review)
+  db.collection('reviews').add(review)
     .then(doc => {
       response.json({ message: `Document ${doc.id} created successfully` })
     })
     .catch(error => {
       response.status(500).json({ error: 'Something went wrong' })
+      console.error(error)
+    })
+})
+
+// Signup route
+app.post('/signup', (request, response) => {
+  const user = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    username: request.body.username
+  }
+
+  let token
+  let userId
+
+  db.doc(`/users/${user.username}`).get()
+    .then(doc => {
+      if (doc.exists) {
+        response.status(400).json({ message: 'This username is already taken' })
+      } else {
+        return firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+      }
+    })
+    .then(data => {
+      userId = data.user.uid
+      return data.user.getIdToken()
+    })
+    .then(userToken => {
+      token = userToken
+      const userCredentials = {
+        username: user.username,
+        email: user.email,
+        createdAt: new Date().toISOString(),
+        userId
+      }
+
+      db.doc(`/users/${user.username}`).set(userCredentials)
+        .then(() => {
+          response.status(201).json({ token })
+        })
+    })
+    .catch(error => {
+      if (error.code === 'auth/email-already-in-use') {
+        response.status(400).json({ error: 'Email already in use' })
+      } else {
+        response.status(500).json({ error: error.code })
+      }
       console.error(error)
     })
 })
