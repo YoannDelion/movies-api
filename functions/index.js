77456d2a3d1b689c88bff7ad55b7d301d09a36b9
Yoann Delion <yoanndelion@gmail.com>
2021-05-24 15:53:48 +0200

    init express with getAllReviews and addReview functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,50 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const express = require('express')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+const app = express()
+
+
+app.get('/reviews', (request, response) => {
+  admin.firestore().collection('reviews').orderBy('createdAt', 'desc').get()
+    .then(data => {
+      const reviews = []
+      data.forEach(doc => {
+        reviews.push({
+          reviewId: doc.id,
+          ...doc.data()
+        })
+      })
+      return response.json(reviews)
+    })
+    .catch(error => {
+      response.status(500).json({ error: 'Something went wrong' })
+      console.error(error)
+    })
+})
+
+app.post('/review', (request, response) => {
+  if (request.method !== 'POST') {
+    response.status(400).json({ error: 'Method not allowed' })
+  }
+  const review = {
+    userId: request.body.userId,
+    movieId: request.body.movieId,
+    review: request.body.review,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString()
+  }
+
+  admin.firestore().collection('reviews').add(review)
+    .then(doc => {
+      response.json({ message: `Document ${doc.id} created successfully` })
+    })
+    .catch(error => {
+      response.status(500).json({ error: 'Something went wrong' })
+      console.error(error)
+    })
+})
+
+// format url as : https://baseurl.com/api/
+exports.api = functions.region('europe-west1').https.onRequest(app)
